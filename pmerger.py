ade756b9f1d4ac6c20ffd2095d2cf361224d5903
sumitgupta3310 <sumitgupta3310@gmail.com>
2025-09-13 17:05:48 +0530

    new feature2 added

--- pmerger.py
+++ pmerger.py
@@ -1,13 +1,21 @@
 import os
 from PyPDF2 import PdfMerger
 
-user_input = input("Enter file path: ")
-print("Merging PDF Files")
-directory = user_input
-merger = PdfMerger()
-for filename in os.listdir(directory):
-    if filename.endswith(".pdf"):
-        merger.append(os.path.join(directory, filename))
-merger.write("C:/Users/Sumit Gupta -SSD PC/Downloads/Ravi/pdfs/merged_output.pdf")
-print("files merged successfully")
-merger.close()
+continue_input = "Y"
+
+while continue_input.casefold() == "y":
+    user_input = input("Enter directory path: ")
+    print("Merging PDF Files")
+    directory = user_input
+
+    merger = PdfMerger()
+    for filename in os.listdir(directory):
+        if filename.endswith(".pdf"):
+            merger.append(os.path.join(directory, filename))
+
+    output_path = os.path.join(directory, "merged_output.pdf")
+    merger.write(output_path)
+    merger.close()
+
+    print(f"Files merged successfully into {output_path}")
+    continue_input = input("Do you wish to continue Y/N: ")
