367403d0dde4bc4d7e95149a72baa29a5764c107
sumitgupta3310 <sumitgupta3310@gmail.com>
2025-09-13 15:53:44 +0530

    PDFMegerApplication

---
+++ PDF Merger.bat.bat
@@ -0,0 +1,4 @@
+@echo off
+call "C:\Users\Sumit Gupta -SSD PC\.virtualenvs\PDF-Merger-F6kyxZzt\Scripts\activate.bat"
+python pmerger.py
+pause
---
+++ pmerger.py
@@ -0,0 +1,13 @@
+import os
+from PyPDF2 import PdfMerger
+
+user_input = input("Enter file pth: ")
+print("Merging PDF Files")
+directory = user_input
+merger = PdfMerger()
+for filename in os.listdir(directory):
+    if filename.endswith(".pdf"):
+        merger.append(os.path.join(directory, filename))
+merger.write("C:/Users/Sumit Gupta -SSD PC/Downloads/Ravi/pdfs/merged_output.pdf")
+print("files merged")
+merger.close()
